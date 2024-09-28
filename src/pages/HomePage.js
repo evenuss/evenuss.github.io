59a1b6ba54ea4b6cd6b841885297c81999379d7b
Jun Pyo <evenusdesilalahi345@gmail.com>
2024-09-28 17:48:33 +0700

    update

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Background from "../assets/background.jpg"
+const HomePage = () =>{
+    return (
+        <div>
+            <img src={Background}/>
+            Home</div>
+    )
+}
+
+export default HomePage
